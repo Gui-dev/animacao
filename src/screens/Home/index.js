2ca1c5114373950cf1c8dbb6fb4efb7c075db766
Gui-dev <adrianon10@gmail.com>
2020-06-09 02:21:05 -0300

    first commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+// import { Container } from './styles'
+
+const Home = () => {
+  return (
+    <SafeAreaView style={ { marginTop: 30 } }>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
