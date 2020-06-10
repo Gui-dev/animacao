7903aaef1987cf84d6e9f51937f3cdf6b90966f0
Gui-dev <adrianon10@gmail.com>
2020-06-10 02:23:08 -0300

    Configurando a animação spring e timing

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
+  Animated,
   View,
   Text,
   Image,
   Alert,
   Dimensions,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
 } from "react-native";
 
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -14,23 +15,51 @@ import styles from './style'
 
 const User = ( { user, onPress } ) => {
 
-  return (
-    <TouchableWithoutFeedback onPress={ onPress }>
-      <View style={styles.userContainer}>
-        <Image style={styles.thumbnail} source={{ uri: user.thumbnail }} />
+  const [ offset, setOffset ] = useState( new Animated.ValueXY( { x: 0, y: 50 } ) )
+  const [ opacity, setOpacity ] = useState( new Animated.Value( 0 ) )
 
-        <View style={[styles.infoContainer, { backgroundColor: user.color }]}>
-          <View style={styles.bioContainer}>
-            <Text style={styles.name}>{user.name.toUpperCase()}</Text>
-            <Text style={styles.description}>{user.description}</Text>
-          </View>
-          <View style={styles.likesContainer}>
-            <Icon name="heart" size={12} color="#FFF" />
-            <Text style={styles.likes}>{user.likes}</Text>
+  useEffect( () => {
+
+    Animated.parallel( [
+
+      Animated.spring( offset.y, {
+        toValue: 0,
+        speed: 5,
+        bounciness: 20
+      } ),
+
+      Animated.timing( opacity, {
+        toValue: 1,
+        duration: 500
+      } )
+
+    ] ).start()
+
+    
+  }, [] )
+
+  return (
+    <Animated.View style={ [
+      { transform: [ ...offset.getTranslateTransform() ] },
+      { opacity }
+    ] }>
+      <TouchableWithoutFeedback onPress={ onPress }>
+        <View style={styles.userContainer}>
+          <Image style={styles.thumbnail} source={{ uri: user.thumbnail }} />
+
+          <View style={[styles.infoContainer, { backgroundColor: user.color }]}>
+            <View style={styles.bioContainer}>
+              <Text style={styles.name}>{user.name.toUpperCase()}</Text>
+              <Text style={styles.description}>{user.description}</Text>
+            </View>
+            <View style={styles.likesContainer}>
+              <Icon name="heart" size={12} color="#FFF" />
+              <Text style={styles.likes}>{user.likes}</Text>
+            </View>
           </View>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </Animated.View>
   )
 }
 
